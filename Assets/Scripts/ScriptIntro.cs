97096c553d798691124d8f7d3d1a0003483bc80d
Lauriane <lauriane.loas@gmail.com>
2017-11-04 10:18:36 +0100

    Intro + reprise fps

---
+++ Assets/Scripts/ScriptIntro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIntro : MonoBehaviour {
+
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		Player.SetActive (false);
+		
+	}
+	
+	// Update is called once per frame
+	public void EndIntro () {
+		Player.SetActive (true);
+		GetComponent<Camera> ().depth = -1;
+
+	}
+}
